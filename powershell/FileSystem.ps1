a08b23cbf4884e39706d3b399197d765f485b499
Junior <PlayTheFallen@gmail.com>
2020-02-04 13:23:19 +0000

    Update FileSystem.ps1

--- powershell/FileSystem.ps1
+++ powershell/FileSystem.ps1
@@ -1,5 +1,11 @@
 # Get all files and folders in a directory (recurse, and map over)
-Get-ChildItem -Recurse | Select-Object FullName
+Get-ChildItem -Recurse
 
 # ^, but directories only
-Get-ChildItem -Recurse | ?{ $_.PSIsContainer } | Select-Object FullName
+Get-ChildItem -Recurse | { $_.PSIsContainer }
+
+# Exclude unity meta files
+Get-ChildItem -Recurse -Exclude *.meta
+
+# Select an object name from a result (or multiple)
+Get ChildItem | Select-Object FullName
